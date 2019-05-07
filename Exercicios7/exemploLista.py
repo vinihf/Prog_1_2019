1a2e729e2c3d58e2d30f38a1cb09b37d17ab5d61
Vinicius H. Ferreira <vinihf@gmail.com>
2019-05-07 15:47:52 -0300

    exemplo

---
+++ Exercicios7/exemploLista.py
@@ -0,0 +1,18 @@
+'''Gerar uma lista'''
+L = list(range(100))
+
+'''Inserir elemento em uma posição específica'''
+L.insert(0,"oi")
+
+'''Inserir ao final da lista'''
+L.append("tchau")
+
+'''Remove primeiro ocorrência do elemento'''
+L.remove("oi")
+
+'''Remove e retorna o elemento'''
+print(L.pop(0))
+
+
+
+print(L)
